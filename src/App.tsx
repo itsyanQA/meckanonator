662ce115402487e9f25817cd5f59266f5bf5610d
stron <yaneliyahu83@gmail.com>
2024-08-03 17:46:32 +0300

    build: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Content } from "@/components/content/content";
+import { Header } from "@/components/header/header";
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <Content />
+    </>
+  );
+}
